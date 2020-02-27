b857b33558c369be72f38be8a8d73ecff6c5785f
Vijai Kalathur <kvijai@gmail.com>
2020-02-27 13:03:15 -0500

    Update example-tekton-pipeline-run.sh

--- scripts/example-tekton-pipeline-run.sh
+++ scripts/example-tekton-pipeline-run.sh
@@ -54,7 +54,7 @@ metadata:
   namespace: kabanero
 spec:
   pipelineRef:
-    name: java-microprofile-build-push-deploy-pipeline
+    name: java-microprofile-build-deploy-pl
   resources:
   - name: git-source
     resourceRef:
@@ -62,6 +62,6 @@ spec:
   - name: docker-image
     resourceRef:
       name: docker-image
-  serviceAccount: kabanero-operator
+  serviceAccountName: kabanero-pipeline
   timeout: 60m
 EOF
